cbc2351583fa41ff1be7bbbe64d78aedade9f236
mymel98 <my458500@naver.com>
2025-06-17 14:47:51 +0900

    Add files via upload

---
+++ 02_select&css함수/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("h1").css({"color":"red"});
+    $(".date").css({"font-size": "80px", "color":"pink", "background":"purple"})
+    $(".para").css({"font-size": 10, "border":"10px solid blue"})
+    $("ul li:last-child").css({"font-size": 48})
+    $("a").css({"padding": 10, "border": "5px solid blue"})
+})
