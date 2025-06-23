df96f876e6752b02110ed4d61c1887aeca0cf8f5
mymel98 <my458500@naver.com>
2025-06-23 16:40:49 +0900

    Add files via upload

---
+++ 10_bannerslide/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.list').slick({
+        autoplay: true,
+        // 한화면에 보일 개수
+        slidesToShow:2,
+        slidesToScroll:1,
+        arrows:false,
+        // 페이지버튼 보이기
+        dots:true,
+    });
+})
