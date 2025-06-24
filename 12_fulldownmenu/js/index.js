87f50530181fd6a9986ca0ce99159a0052522f42
mymel98 <my458500@naver.com>
2025-06-24 16:09:31 +0900

    Add files via upload

--- 12_fulldownmenu/js/index.js
+++ 12_fulldownmenu/js/index.js
@@ -1,10 +1,12 @@
 $(function(){
     $("ul.gnb").mouseenter(function(){
-        $("#header_wrap").stop().animate({"height":380},300)
+        $("#header_wrap").stop().animate({"height":380},500);
         $(".depth2").show();
+        $("#header_wrap").css({'border-bottom': '1px solid #c4c4c4'});
     })
-    $("ul.gnb").mouseleave(function(){
-        $("#header_wrap").stop().animate({"height":110},300)
+    $("#header_wrap").mouseleave(function(){
+        $("#header_wrap").stop().animate({"height":110},500);
         $(".depth2").hide();
+        $("#header_wrap").css({'border-bottom': 'none'});
     })
 })
