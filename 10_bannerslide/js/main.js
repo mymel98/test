c4e49f72a9586cfc02f6d2bbb69dde89b226e15b
mymel98 <my458500@naver.com>
2025-06-30 16:45:57 +0900

    Add files via upload

--- 10_bannerslide/js/main.js
+++ 10_bannerslide/js/main.js
@@ -2,10 +2,27 @@ $(function () {
     $('.list').slick({
         autoplay: true,
         // 한화면에 보일 개수
-        slidesToShow:2,
-        slidesToScroll:1,
-        arrows:false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
         // 페이지버튼 보이기
-        dots:true,
+        dots: true,
     });
+    // 일시정지버튼을 누르면 플레이버튼으로 바뀌고, 플레이버튼을 누르면 다시 재생버튼
+    $(".btn_pause").click(function () {
+        $(".btn_pause").toggleClass("on");
+    })
+    // addClass(프로그램 자동실행), removeClass(프로그램 자동삭제)
+    let sw = true;
+    $(".btn_pause").click(function () {
+        if (sw == true) {
+            $(".btn_pause").addClass("on");
+            $(".list").slick("slickPause");
+            sw = false;
+        } else {
+            $(".btn_pause").removeClass("on");
+            $(".list").slick("slickPlay");
+            sw = true;
+        }
+    })
 })
