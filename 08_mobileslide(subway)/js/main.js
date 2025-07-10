df83ea2c55e23db409cea664715015772c88c012
mymel98 <my458500@naver.com>
2025-07-10 17:15:18 +0900

    Add files via upload

--- 08_mobileslide(subway)/js/main.js
+++ 08_mobileslide(subway)/js/main.js
@@ -24,5 +24,27 @@ $(function(){
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+  
 });
+$('.banner_slide').slick({
+  autoplay: true,
+  slidesToShow: 2,
+  // 좌우버튼 숨기기
+  arrows: false,
+  // 페이지버튼 보이기
+  dots: true
+});
+// 모바일 메뉴 클릭하면
+$(".m_gnb>ul>li>a" ).click(function(){
+  $(this).next().slideToggle().parent().siblings().find(".depth2").slideUp()
+})
+// 햄버거 버튼을 클릭하면
+$(".m_btn").click(function(){
+  $(".cover").fadeIn()
+  $(".m_side_wrap").animate({"left":0}, 300)
+})
+$(".m_close").click(function(){
+  $(".m_side_wrap").animate({"left":'-100%'},300)
+  $(".cover").fadeOut()
+})
 })
